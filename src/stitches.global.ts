59b4aa9600b23421d64c21a73277e4e5ee268ae8
brendansiraky <brendansirakydeveloper@gmail.com>
2022-11-10 20:28:03 +1000

    added document

--- src/stitches.global.ts
+++ src/stitches.global.ts
@@ -4,7 +4,7 @@ type Direction = 'row' | 'column'
 type Alignment = 'start' | 'center' | 'end' | 'between' | 'around'
 type Flexer = `${Direction}-${Alignment}-${Alignment}`
 
-export const { styled, css } = createStitches({
+export const { styled, css, getCssText } = createStitches({
     theme: {
         space: {
             xs: '5px',
